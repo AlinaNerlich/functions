f08b73cf7fd24bcad98170ff32f5744a9c3b1826
Alina Nerlich <alina.nerlich@gmx.de>
2020-06-21 19:16:20 +0200

    fehler steps counter

--- test.py
+++
@@ -1,7 +0,0 @@
-def stepCount():
-    distance = float(input("How far did you walk(in meters)?: "))
-    step = float(input("How long is your step(in cm.)?: "))
-    return distance//step
-
-a = stepCount()
-print("You made " + str(int(a)) + " steps during your walk.")
